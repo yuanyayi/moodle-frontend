e0af818e17b92ea99dfe75f1d07488ecf84264bb
CrystalY <callmecrystal@163.com>
2025-12-26 11:49:44 +0800

    feat：取消登出

--- src/components/GlobalHeader/AvatarDropdown.vue
+++ src/components/GlobalHeader/AvatarDropdown.vue
@@ -1,32 +1,30 @@
 <template>
-  <a-dropdown v-if="currentUser && currentUser.name" placement="bottomRight">
+  <!-- <a-dropdown v-if="currentUser && currentUser.name" placement="bottomRight"> -->
+  <div v-if="currentUser && currentUser.name" placement="bottomRight">
     <span class="ant-pro-account-avatar">
-      <a-avatar
-        size="small"
-        src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
-        class="antd-pro-global-header-index-avatar"
-      />
+      <a-avatar size="small" src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png" class="antd-pro-global-header-index-avatar" />
       <span>{{ currentUser.name }}</span>
     </span>
-    <template v-slot:overlay>
+    <!-- <template v-slot:overlay>
       <a-menu class="ant-pro-drop-down menu" :selected-keys="[]">
         <a-menu-item key="logout" @click="handleLogout">
           <a-icon type="logout" />
           退出登录
         </a-menu-item>
       </a-menu>
-    </template>
-  </a-dropdown>
+    </template> -->
+    <!-- </a-dropdown> -->
+  </div>
   <span v-else>
     <a-spin size="small" :style="{ marginLeft: 8, marginRight: 8 }" />
   </span>
 </template>
 
 <script>
-import { Modal } from 'ant-design-vue'
+import { Modal } from "ant-design-vue";
 
 export default {
-  name: 'AvatarDropdown',
+  name: "AvatarDropdown",
   props: {
     currentUser: {
       type: Object,
@@ -39,28 +37,28 @@ export default {
   },
   methods: {
     handleToCenter() {
-      this.$router.push({ path: '/account/center' })
+      this.$router.push({ path: "/account/center" });
     },
     handleToSettings() {
-      this.$router.push({ path: '/account/settings' })
+      this.$router.push({ path: "/account/settings" });
     },
     handleLogout(e) {
       Modal.confirm({
-        title: this.$t('layouts.usermenu.dialog.title'),
-        content: this.$t('layouts.usermenu.dialog.content'),
+        title: this.$t("layouts.usermenu.dialog.title"),
+        content: this.$t("layouts.usermenu.dialog.content"),
         onOk: () => {
           // return new Promise((resolve, reject) => {
           //   setTimeout(Math.random() > 0.5 ? resolve : reject, 1500)
           // }).catch(() => console.log('Oops errors!'))
-          return this.$store.dispatch('Logout').then(() => {
-            this.$router.push({ name: 'login' })
-          })
+          return this.$store.dispatch("Logout").then(() => {
+            this.$router.push({ name: "login" });
+          });
         },
         onCancel() {},
-      })
+      });
     },
   },
-}
+};
 </script>
 
 <style lang="less" scoped>
