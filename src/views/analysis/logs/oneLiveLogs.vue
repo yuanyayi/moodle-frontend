e61ac6b3f1dcf9df3a9a0d0ad22bfd1120bcd13a
CrystalY <callmecrystal@163.com>
2026-02-27 17:01:32 +0800

    feat：style

--- src/views/analysis/logs/oneLiveLogs.vue
+++ src/views/analysis/logs/oneLiveLogs.vue
@@ -76,7 +76,7 @@ export default {
         pageSize: 10,
         showSizeChanger: true,
         pageSizeOptions: ["10", "20", "50", "100"],
-        showTotal: (total) => `共 ${total} 条记录`,
+        showTotal: (total) => `共 ${total} 条数据`,
       },
       columns: [
         {
